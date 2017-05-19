1ac4f1c6799c33ed8c61df90ecc2e02e87881610
ommhoa <omar.osman.asd@gmail.com>
2017-05-19 21:57:41 +0200

    remove unneeded paramchecking

--- middleware/registration/checkRegistrationParams.js
+++ middleware/registration/checkRegistrationParams.js
@@ -9,8 +9,13 @@ module.exports = function (objectrepository) {
 
     return function (req, res, next) {
     	console.log("checkRegistrationParams");
-        if (!req.body.userId || !req.body.password || !req.body.name || !req.body.email || !req.body.city || !req.body.address
-            || !req.body.postal || !req.body.title || !req.body.writer || !req.body.pageCount || !req.body.state || !req.body.language) {
+        // if (!req.body.userId || !req.body.password || !req.body.name || !req.body.email || !req.body.city || !req.body.address
+        //     || !req.body.postal || !req.body.title || !req.body.writer || !req.body.pageCount || !req.body.state || !req.body.language) {
+        //     req.session.error = "Minden szükséges mező kitöltése kötezelő!";
+        //     return res.redirect("/registration");
+        // }
+        if (!req.body.userId || !req.body.password || !req.body.name || !req.body.city 
+           	|| !req.body.title || !req.body.writer || !req.body.state) {
             req.session.error = "Minden szükséges mező kitöltése kötezelő!";
             return res.redirect("/registration");
         }
